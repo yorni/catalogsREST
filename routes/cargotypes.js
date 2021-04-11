ab29f786ceb35cb94f1816a52f080bb49a2be083
Yaroslav Kulik <ygkulik@gmail.com>
2021-04-11 18:19:29 +0300

    cargo types added

--- routes/cargotypes.js
+++ routes/cargotypes.js
@@ -127,12 +127,25 @@ async function getListCargoTypes(req, res, next) {
     return res.status(500).json({ message: err.message });
   }
 }
-
 function isEmptyObject(obj) {
-  console.log(obj);
-  for (var prop in obj) {
-    console.log(prop);
-    if (obj.hasOwnProperty(prop)) return false;
+  // null and undefined are "empty"
+  if (obj == null) return true;
+
+  // Assume if it has a length property with a non-zero value
+  // that that property is correct.
+  if (obj.length > 0) return false;
+  if (obj.length === 0) return true;
+
+  // If it isn't an object at this point
+  // it is empty, but it can't be anything *but* empty
+  // Is it empty?  Depends on your application.
+  if (typeof obj !== "object") return true;
+
+  // Otherwise, does it have any properties of its own?
+  // Note that this doesn't handle
+  // toString and valueOf enumeration bugs in IE < 9
+  for (var key in obj) {
+    if (hasOwnProperty.call(obj, key)) return false;
   }
 
   return true;
